49edbc73145a5a85a412426d1bd8180338eacf76
sahil-sangtani <sahilsangtani01@gmail.com>
2024-09-01 13:35:42 +0530

    expiry
    expiry date

--- server.js
+++ server.js
@@ -1,3 +1,47 @@
+// import express from "express";
+// import colors from "colors";
+// import dotenv from "dotenv";
+// import morgan from "morgan";
+// import connectDB from "./config/db.js";
+// import authRoutes from "./routes/authRoute.js";
+// import categoryRoutes from "./routes/categoryRoutes.js";
+// import productRoutes from "./routes/productRoutes.js";
+// import cors from "cors";
+// //import path from "path";
+
+// //configure env
+// dotenv.config();
+
+// //database config
+// connectDB();
+
+// //rest object
+// const app = express();
+
+// // middlewares
+// app.use(cors());
+// app.use(express.json());
+// app.use(morgan("dev"));
+// //app.use(express.static(path.join(__dirname, "./client/build")));
+
+// //routes
+// app.use("/api/v1/auth", authRoutes);
+// app.use("/api/v1/category", categoryRoutes);
+// app.use("/api/v1/product", productRoutes);
+
+// const PORT = process.env.PORT || 8080;
+
+// app.get("/", (req, res) => {
+//   res.send("Welcome to Sahil Medicos");
+// });
+// // app.use("*", function (req, res) {
+// //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// // });
+
+// app.listen(PORT, () => {
+//   console.log(`APP listening on port: ${PORT}`);
+// });
+
 import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
@@ -7,13 +51,17 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-//import path from "path";
+import path from "path";
+import { fileURLToPath } from "url";
 
 //configure env
 dotenv.config();
 
 //database config
 connectDB();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
@@ -22,7 +70,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-//app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -31,12 +79,9 @@ app.use("/api/v1/product", productRoutes);
 
 const PORT = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
-  res.send("Welcome to Sahil Medicos");
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-// app.use("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
 
 app.listen(PORT, () => {
   console.log(`APP listening on port: ${PORT}`);
